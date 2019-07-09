a150580f08b01968d3dcd844e8f94dda77829039
Ubuntu <ubuntu@ip-172-31-27-148.eu-west-1.compute.internal>
2019-07-09 10:11:25 +0000

    venues map finalised

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,78 +1,154 @@
 queue()
-    .defer(d3.csv, "data/all_teams.csv")
+    .defer(d3.json, "data/all_teams.json")
     .await(makeGraphs);
-    
+
 function makeGraphs(error, teamRosters) {
     var ndx = crossfilter(teamRosters);
-    
+
     initMap();
+    player_by_state(ndx);
     player_position(ndx);
     player_by_age(ndx);
-    
+
     dc.renderAll();
 }
 
 // ------------------------------------------------------------ VENUES MAP -------------------------------------------------
+function initMap() {
+    initVenuesMap();
+    // initStatesMap();
+}
 
-       function initMap() {
+function initVenuesMap() {
 
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 3,
-                center: { lat: -28.024, lng: 140.887 }
-            });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: { lat: 39.115413, lng: -94.610070 },
+    });
 
-            // Create an array of alphabetical characters used to label the markers.
-            var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // Create an array of alphabetical characters used to label the markers.
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-            // Add some markers to the map.
-            // Note: The code uses the JavaScript Array.prototype.map() method to
-            // create an array of markers based on a given "locations" array.
-            // The map() method here has nothing to do with the Google Maps API.
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
 
-            // Add a marker clusterer to manage the markers.
-            var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-        }
-        var locations = [
-            { lat: -31.563910, lng: 147.154312 },
-            { lat: -33.718234, lng: 150.363181 },
-            { lat: -33.727111, lng: 150.371124 },
-            { lat: -33.848588, lng: 151.209834 },
-            { lat: -33.851702, lng: 151.216968 },
-            { lat: -34.671264, lng: 150.863657 },
-            { lat: -35.304724, lng: 148.662905 },
-            { lat: -36.817685, lng: 175.699196 },
-            { lat: -36.828611, lng: 175.790222 },
-            { lat: -37.750000, lng: 145.116667 },
-            { lat: -37.759859, lng: 145.128708 },
-            { lat: -37.765015, lng: 145.133858 },
-            { lat: -37.770104, lng: 145.143299 },
-            { lat: -37.773700, lng: 145.145187 },
-            { lat: -37.774785, lng: 145.137978 },
-            { lat: -37.819616, lng: 144.968119 },
-            { lat: -38.330766, lng: 144.695692 },
-            { lat: -39.927193, lng: 175.053218 },
-            { lat: -41.330162, lng: 174.865694 },
-            { lat: -42.734358, lng: 147.439506 },
-            { lat: -42.734358, lng: 147.501315 },
-            { lat: -42.735258, lng: 147.438000 },
-            { lat: -43.999792, lng: 170.463352 }
-        ];
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
+var locations = [
+    { lat: 33.757307, lng: -84.396324 }, // State Farm Arena, Atlanta, Georgia
+    { lat: 42.366230, lng: -71.062146 }, // TD Garden, Boston, Massachussetts
+    { lat: 40.682523, lng: -73.976094 }, // Barclays Center, Brooklyn, New York
+    { lat: 35.225188, lng: -80.839306 }, // Spectrum Center, Charlotte, North Carolina
+    { lat: 41.880694, lng: -87.674173 }, // United Center, Chicago, Illinois
+    { lat: 41.496482, lng: -81.688214 }, // Rocket Mortgage FieldHouse, Cleveland, Ohio
+    { lat: 32.790727, lng: -96.811091 }, // American Airlines Center, Dallas, Texas
+    { lat: 39.748663, lng: -105.007710 }, // Pepsi Center, Denver, Colorado
+    { lat: 42.341011, lng: -83.054971 }, // Little Caesars Arena, Detroit, Michigan
+    { lat: 37.750305, lng: -122.202961 }, // Oracle Arena, Oakland, California
+    { lat: 29.750764, lng: -95.362101 }, // Toyota Center, Houston, Texas
+    { lat: 39.763890, lng: -86.155460 }, // Bankers Life Fieldhouse, Indianapolis, Indiana
+    { lat: 34.043027, lng: -118.267254 }, // Staples Center, Los Angeles, California
+    { lat: 35.138256, lng: -90.050503 }, // FedEx Forum, Memphis, Tennessee
+    { lat: 25.781424, lng: -80.186969 }, // American Airlines Arena, Miami, Florida
+    { lat: 43.045052, lng: -87.916793 }, // Fiserv Forum, Milwaukee, Wisconsin
+    { lat: 44.979318, lng: -93.275697 }, // Target Center, Minneapolis, Minnesota
+    { lat: 29.949038, lng: -90.082055 }, // Smoothie King Center, New Orleans, Louisiana
+    { lat: 40.750517, lng: -73.993439 }, // Madison Square Garden, New York City, New York
+    { lat: 35.463444, lng: -97.515096 }, // Chesapeake Energy Arena, Oklahoma City, Oklahoma
+    { lat: 28.539229, lng: -81.383856 }, // Amway Center, Orlando, Florida
+    { lat: 39.901311, lng: -75.171957 }, // Wells Fargo Center, Philadelphia, Pennsylvania
+    { lat: 33.445742, lng: -112.071205 }, // Talking Stick Resort Arena, Phoenix, Arizona
+    { lat: 45.531929, lng: -122.666866 }, // Moda Center, Portland, Oregon
+    { lat: 29.427153, lng: -98.437490 }, // AT&T Center, San Antonio, Texas
+    { lat: 38.580213, lng: -121.499660 }, // Golden 1 Center, Sacramento, California
+    { lat: 43.643505, lng: -79.379106 }, // Scottiabank Arena, Toronto, Ontario
+    { lat: 40.768298, lng: -111.901088 }, // Vivint Smart Home Arena, Salt Lake City, Utah
+    { lat: 38.898114, lng: -77.020992 } // Capital One Arena, Washington, District of Columbia
+
+];
+
+//  ---------------------------------------------------- STATES MAP --------------------------------------------------------
+
+// var mapStyle = [{
+//     'stylers': [{ 'visibility': 'off' }]
+// }, {
+//     'featureType': 'landscape',
+//     'elementType': 'geometry',
+//     'stylers': [{ 'visibility': 'on' }, { 'color': '#fcfcfc' }]
+// }, {
+//     'featureType': 'water',
+//     'elementType': 'geometry',
+//     'stylers': [{ 'visibility': 'on' }, { 'color': '#bfd4ff' }]
+// }];
+
+// function initStatesMap() {
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 4,
+//         center: { lat: 39.115413, lng: -94.610070 },
+//         styles: mapStyle
+//     });
+
+//     // set up the style rules and events for google.maps.Data
+//     map.data.setStyle(styleFeature);
+//     map.data.addListener('mouseover', mouseInToRegion);
+//     map.data.addListener('mouseout', mouseOutOfRegion);
+
+//     // state polygons only need to be loaded once, do them now
+//     loadMapShapes();
+// }
+
+// // Loads the state boundary polygons from a GeoJSON source. 
+// function loadMapShapes() {
+//     // load US state outline polygons from a GeoJson file
+//     map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/states.js', { idPropertyName: 'STATE' });
+// }
+
+// var states_map_dim = parseFloat("players__player__birth_state");
+
+// map.data
+//     .get(states_map_dim);
+
+
+
+
+// -------------------------------------------------------- PLAYER BY STATE PIE CHART ----------------------------------------
+
+function player_by_state(ndx) {
+    var state_dim = ndx.dimension(dc.pluck("players__player__birth_state"));
+    var group = state_dim.group();
+
+    dc.pieChart("#player-state")
+        .width(800)
+        .height(800)
+        .radius(200)
+        .transitionDuration(1500)
+        .externalLabels(100)
+        .drawPaths(true)
+        .renderTitle(true)
+        .minAngleForLabel(0)
+        .dimension(state_dim)
+        .group(group);
+}
+
 // ----------------------------------------------------------- PLAYER POSITION BAR CHART ------------------------------------
 
 function player_position(ndx) {
     var position_dim = ndx.dimension(dc.pluck("players__player__primary_position"));
     var position_group = position_dim.group();
-    
+
     dc.barChart("#player-position")
         .width(400)
         .height(400)
-        .margins({ top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(position_dim)
         .group(position_group)
         .transitionDuration(500)
@@ -88,11 +164,11 @@ function player_position(ndx) {
 function player_by_age(ndx) {
     var age_dim = ndx.dimension(dc.pluck("players__player__age"));
     var age_group = age_dim.group();
-    
+
     dc.barChart("#player-by-age")
         .width(600)
         .height(600)
-        .margins({ top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(age_dim)
         .group(age_group)
         .transitionDuration(500)
